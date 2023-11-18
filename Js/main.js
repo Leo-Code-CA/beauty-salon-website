db81836209dc99c9265a48199ecb9ae1831512be
Leo-Code-CA <144139247+Leo-Code-CA@users.noreply.github.com>
2023-11-17 20:20:06 -0500

    Start to work on the navbar

---
+++ Js/main.js
@@ -0,0 +1,9 @@
+// document.getElementById('searchbartg').onclick = function() {
+//     let btn = document.getElementById('searchBar');
+
+//     if (btn.style.display === 'none') {
+//         btn.style.display = 'block';
+//     } else {
+//         btn.style.display = 'none';
+//     }
+// }
