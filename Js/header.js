78bca9cc9272b656ea73320cc2bc071073d34d55
Leo-Code-CA <144139247+Leo-Code-CA@users.noreply.github.com>
2023-12-06 21:55:31 -0500

    beginning of the homepage

--- Js/header.js
+++ Js/header.js
@@ -23,8 +23,35 @@ function handleFetchHeader() {
         })
         .then(() => {
             focusState('searchInput', 'searchGroup', 'navbar__input-group--focus');
-        });
+        })
+        .then(() => handleMainMargin());
 
 }
 
 handleFetchHeader();
+
+function handleMainMargin() {
+    const navHeight = document.getElementById('nav').offsetHeight;
+    const main = document.getElementById('mainhome');
+
+    main.style.marginTop = navHeight + "px";
+    main.style.height = `calc(100vh - ${navHeight}px)`;
+
+}
+
+
+
+
+//  // Focus state function - DESIGN
+//  const focusState = (firstElemID, secondElemID, className) => {
+
+//  const focusedElem = document.getElementById(firstElemID);
+//  const switchClassElem = document.getElementById(secondElemID);
+
+//  focusedElem.onfocus = () => switchClassElem.classList.add(className);
+
+//  focusedElem.onblur = () => switchClassElem.classList.remove(className);
+//  }
+
+//  // Call the focus state function for the navbar input group (search bar)
+//  focusState('searchInput', 'searchGroup', 'navbar__input-group--focus');
--- Js/main.js
+++ Js/main.js
@@ -1,33 +1,23 @@
-// Update the public side of the website with the most recent content - FUNCTIONALITY
-function handleLoadHome() {
+// Update the public side of the website with the most recent content - FUNCTIONALITY - REACTIVATE WHEN READY
+// function handleLoadHome() {
 
-    if (localStorage.getItem('webta') !== null) {
-         document.getElementById('webp').innerHTML = localStorage.getItem('webta');
-     }
+//     if (localStorage.getItem('webta') !== null) {
+//          document.getElementById('webp').innerHTML = localStorage.getItem('webta');
+//      }
 
-    localStorage.setItem("webp", document.getElementById('webp').innerHTML);
-    console.log(`webp value : ${localStorage.getItem("webp")}`);
-    console.log(`webta value : ${localStorage.getItem("webta")}`);
+//     localStorage.setItem("webp", document.getElementById('webp').innerHTML);
+//     console.log(`webp value : ${localStorage.getItem("webp")}`);
+//     console.log(`webta value : ${localStorage.getItem("webta")}`);
 
-    }
+//     }
 
-    document.getElementById('bodyhome').addEventListener("load", handleLoadHome());
+//     document.getElementById('bodyhome').addEventListener("load", handleLoadHome());
 
 
 
-//  // Focus state function - DESIGN
-//  const focusState = (firstElemID, secondElemID, className) => {
 
-//  const focusedElem = document.getElementById(firstElemID);
-//  const switchClassElem = document.getElementById(secondElemID);
 
-//  focusedElem.onfocus = () => switchClassElem.classList.add(className);
 
-//  focusedElem.onblur = () => switchClassElem.classList.remove(className);
-//  }
-
-//  // Call the focus state function for the navbar input group (search bar)
-//  focusState('searchInput', 'searchGroup', 'navbar__input-group--focus');
 
 
 
