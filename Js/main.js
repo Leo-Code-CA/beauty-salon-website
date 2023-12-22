46d4e12348ba6f660e08d2351e48c96d41bf7714
Leo-Code-CA <144139247+Leo-Code-CA@users.noreply.github.com>
2023-12-21 20:53:05 -0500

    regroup files

--- Js/main.js
+++ Js/main.js
@@ -1,5 +1,6 @@
-const KEY = require('./data.js');
-console.log(KEY);
+require('dotenv').config();
+
+const KEY = process.env.API_KEY;
 
 // Update the public side of the website with the most recent content - FUNCTIONALITY - REACTIVATE WHEN READY
 // function handleLoadHome() {
@@ -17,7 +18,7 @@ console.log(KEY);
 //     document.getElementById('bodyhome').addEventListener("load", handleLoadHome());
 
 /////////////////////////////////// REVIEWS FETCH
-
+if (typeof document !== 'undefined') {
 
 (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
     key: KEY,
@@ -68,6 +69,8 @@ reviews.map((review, i) => {
 }
 
 getPlaceDetails();
+
+}
 
 ///////////////////////////////////
 
