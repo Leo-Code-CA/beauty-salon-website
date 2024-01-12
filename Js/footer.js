f3165b173f147759ca15a50ddc83b43f541df410
Leo-Code-CA <144139247+Leo-Code-CA@users.noreply.github.com>
2024-01-11 21:48:05 -0500

    html and css cleanup done / next is Js

--- Js/footer.js
+++ Js/footer.js
@@ -1,3 +1,16 @@
+// Fetch footer function
+
+async function fetchFooter() {
+    
+    const response = await fetch('footer.html');
+    const html = await response.text();
+    document.getElementById('footer').innerHTML = html;
+    initMap();
+}
+
+fetchFooter();
+
+
 
 (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
         key: "",
@@ -10,15 +23,6 @@
       let marker;
       let infowindow;
     
-    async function fetchFooter() {
-    
-        const response = await fetch('footer.html');
-        const html = await response.text();
-        document.getElementById('footer').innerHTML = html;
-        initMap();
-    }
-    
-    fetchFooter();
     
     
     
@@ -39,7 +43,7 @@
         map.setCenter({lat: 47.81035232543945, lng: 4.6635764});
     
         const icon = google.maps.Icon = {
-            url: '/images/homepage/pin.svg'
+            url: '/images/homepage/pin_icon.svg'
         }
     
         marker = new Marker({
