dd5d888ed0d5cae4592db507b0c7921c7f386c30
Leo-Code-CA <144139247+Leo-Code-CA@users.noreply.github.com>
2024-02-10 22:08:25 -0500

    not done yet

--- Js/jsBackup.js
+++ Js/jsBackup.js
@@ -8,6 +8,60 @@
 
 // Variables
 
+// let scrollDirection;
+// let lastScroll = 0;
+
+// window.addEventListener('scroll', function() {
+
+//     let currentScroll = window.scrollY;
+
+//     if (currentScroll > 0 && currentScroll >= lastScroll) {
+//         scrollDirection = "down";
+//         lastScroll = currentScroll;
+//     } else {
+//         scrollDirection = "up";
+//         lastScroll = currentScroll;
+//     }
+
+// })
+
+// const summary = document.querySelector('.summary__box:nth-child(1)');
+// const carouselSection = document.querySelector(".carousel__section");
+
+// const options = {
+//     root: null,
+//     threshold: 1
+// }
+
+// function handleObserve(entries, observer) {
+
+//     entries.forEach(entry => {
+
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add("summary__box--highlight");
+//             entry.target.scrollIntoView(false);
+//         } 
+
+//         // else {
+//         //     entry.target.classList.remove("summary__box--highlight");
+//         // }
+        
+
+//     })
+
+// }
+
+// const observer = new IntersectionObserver(handleObserve, options);
+// observer.observe(summary);
+
+
+
+
+
+
+
+
+
 
 const visisbleScrollBox = document.querySelector('.summary__animationBox');
 const fullScrollBox = document.querySelector('.summary');
@@ -28,6 +82,15 @@ visisbleScrollBox.addEventListener('scroll', () => {
     const lengthFromTop = visisbleScrollBox.scrollTop;
     const scrollArea = fullBoxHeight - visibleBoxHeight;
     const scrollPercentage = Math.ceil(lengthFromTop) / (fullBoxHeight - visibleBoxHeight) * 100;
+
+    // increase the speed of the scroll bound animation so one element don't take all the scroll height available
+    const fastScroll = ((scrollPercentage) * 9);
+
+    // TEST
+    const elemScrollPercentage = Math.ceil(lengthFromTop) / ((fullBoxHeight / 3) - (visibleBoxHeight / 3)) * 100;
+    // console.log(elemScrollPercentage)
+
+
 
     // reset the elements - hidden
     if (lengthFromTop === 0) {
@@ -51,55 +114,51 @@ visisbleScrollBox.addEventListener('scroll', () => {
         lastScroll = currentScroll;
     }
 
-    const width = elemOne.getBoundingClientRect().width;
-    const windowWidth = window.innerWidth;
-    const widthDifference = Math.round((windowWidth - width) / 2);
-    const widthDifferencePercentage = windowWidth / widthDifference;
-
-    // let showmeyourTranlaslate;
-
+    // TEST: LET SOME TIME TO MODIFY THE ELEMENT
+    // instead of 33% for each, just give 25%
+    // 25 / 400 = 0.0625
+    // 33% was 33% of the total scrollArea = 2201 / 3.3 = 666.9 (for each elem)
+    // 25% is 25% of the total scrollArea = 2201 / 4 = 550.25 (for each elem)
+    // AND ALSO 25% left for immobile element 
+    // 25% / 3 so 550.25 / 3 = 183.42
 
     // animate the first element
     if (lengthFromTop <= (scrollArea / 3)) {
 
-        const translatePercentage = -100 + (scrollPercentage / 0.05689); // max is 300 (from -100 to 300)
+        // elemOne.style.transform = `translate(-100%) translate(${fastScroll}%)`
+        // console.log(-100 + fastScroll)
 
+        // TEST
+        // 0.0825 x 400 = 33 comes from 33 / 400 = 0.0825
+        // take percentage and divide it by 0.0825
+        const translatePercentage = -100 + (scrollPercentage / 0.0825);
+
+
+        const width = elemOne.getBoundingClientRect().width;
+        const left = Math.round(elemOne.getBoundingClientRect().left);
+        const windowWidth = window.innerWidth;
+        const widthDifference = Math.round((windowWidth - width) / 2);
+        const widthDifferencePercentage = windowWidth / widthDifference;
+
+        // console.log(widthDifferencePercentage)
+        // console.log(translatePercentage)
+
+
+        
         if (translatePercentage < widthDifferencePercentage) {
 
             elemOne.style.transform = `translate(${translatePercentage}%)`
-            showmeyourTranlaslate = translatePercentage;
-   
 
         } else if (translatePercentage >= widthDifferencePercentage && translatePercentage < widthDifferencePercentage + 180) {
 
-            const difference = translatePercentage - widthDifferencePercentage;
-            const differencePercentage = difference / 180 * 100;
-
-
-            const currentHeight = elemOne.clientHeight;
-            const goalHeight = window.innerHeight / 100 * 60;
-            const differenceHeight = goalHeight - currentHeight; // 205
-
-            
-
-            // find a way to increase depending on the scroll percentage (out of 180);
-
-            
-
-
-
-
-
-
-
-            showmeyourTranlaslate = widthDifferencePercentage;
+            console.log(translatePercentage)
             
         } else {
 
-            elemOne.style.transform = `translate(${translatePercentage - 180}%)`
-            showmeyourTranlaslate = translatePercentage - 180;
-
+            console.log("after")
         }
+
+
 
         if (scrollDirection === 'down') {
             elemTwo.style.transform = `translate(-100%)`
@@ -110,31 +169,16 @@ visisbleScrollBox.addEventListener('scroll', () => {
         }
 
 
-        console.log(showmeyourTranlaslate)
-
-
     }
 
     // animate the second element
-
     else if (lengthFromTop <= (scrollArea / 3) * 2) {
 
-        const translatePercentage = -100 + (scrollPercentage / 0.05689 - 580);
+        // elemOne.style.transform = `translate(-100%) translate(300%)`;
+        // elemTwo.style.transform = `translate(-100%) translate(${fastScroll - 300}%)`;
 
-
-        if (translatePercentage < widthDifferencePercentage) {
-
-            elemTwo.style.transform = `translate(${translatePercentage}%)`
-
-        } else if (translatePercentage >= widthDifferencePercentage && translatePercentage < widthDifferencePercentage + 180) {
-
-            // DO SOMETHING
-            
-        } else {
-
-            elemTwo.style.transform = `translate(${translatePercentage - 180}%)`
-
-        }
+        const translatePercentage = -100 + (scrollPercentage / 0.0825 - 400);
+        elemTwo.style.transform = `translate(${translatePercentage}%)`
 
         if (scrollDirection === 'down') {
             elemOne.style.transform = `translate(300%)`
@@ -144,27 +188,17 @@ visisbleScrollBox.addEventListener('scroll', () => {
             elemThree.style.transform = `translate(300%)`
         }
 
+
     }
 
     // animate the third element
     else if (lengthFromTop < scrollArea) {
 
-        const translatePercentage = -100 + (scrollPercentage / 0.05689 - 1160);
+        // elemTwo.style.transform = `translate(-100%) translate(300%)`;
+        // elemThree.style.transform = `translate(-100%) translate(${fastScroll - 600}%)`;
 
-
-        if (translatePercentage < widthDifferencePercentage) {
-
-            elemThree.style.transform = `translate(${translatePercentage}%)`
-
-        } else if (translatePercentage >= widthDifferencePercentage && translatePercentage < widthDifferencePercentage + 180) {
-
-            // DO SOMETHING
-            
-        } else {
-
-            elemThree.style.transform = `translate(${translatePercentage - 180}%)`
-
-        }
+        const translatePercentage = -100 + (scrollPercentage / 0.0825 - 800);
+        elemThree.style.transform = `translate(${translatePercentage}%)`
 
         if (scrollDirection === 'down') {
             elemOne.style.transform = `translate(300%)`
@@ -178,19 +212,6 @@ visisbleScrollBox.addEventListener('scroll', () => {
     }
 
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Take care of the behaviour of the rest of the page
 
@@ -265,85 +286,6 @@ window.addEventListener('scroll', () => {
     }
 
 });
-
-
-
-// TEST: LET SOME TIME TO MODIFY THE ELEMENT
-// 0.0825 x 400 = 33 comes from 33 / 400 = 0.0825
-// take percentage and divide it by 0.0825
-// instead of 33% for each, just give 25%
-// 25 / 400 = 0.0625
-// 33% was 33% of the total scrollArea = 2201 / 3.3 = 666.9 (for each elem)
-// 25% is 25% of the total scrollArea = 2201 / 4 = 550.25 (for each elem)
-// AND ALSO 25% left for immobile element 
-// 25% / 3 so 550.25 / 3 = 183.42
-
-
-
-// ANOTHER WORKING WAY:
-// elemOne.style.transform = `translate(-100%) translate(${fastScroll}%)`
-// elemOne.style.transform = `translate(-100%) translate(300%)`;
-// elemTwo.style.transform = `translate(-100%) translate(${fastScroll - 300}%)`;
-// elemTwo.style.transform = `translate(-100%) translate(300%)`;
-// elemThree.style.transform = `translate(-100%) translate(${fastScroll - 600}%)`;
-
-// increase the speed of the scroll bound animation so one element don't take all the scroll height available
-// const fastScroll = ((scrollPercentage) * 9);
-
-
-// ANOTHER TEST: 
-// const elemScrollPercentage = Math.ceil(lengthFromTop) / ((fullBoxHeight / 3) - (visibleBoxHeight / 3)) * 100;
-
-
-// let scrollDirection;
-// let lastScroll = 0;
-
-// window.addEventListener('scroll', function() {
-
-//     let currentScroll = window.scrollY;
-
-//     if (currentScroll > 0 && currentScroll >= lastScroll) {
-//         scrollDirection = "down";
-//         lastScroll = currentScroll;
-//     } else {
-//         scrollDirection = "up";
-//         lastScroll = currentScroll;
-//     }
-
-// })
-
-// const summary = document.querySelector('.summary__box:nth-child(1)');
-// const carouselSection = document.querySelector(".carousel__section");
-
-// const options = {
-//     root: null,
-//     threshold: 1
-// }
-
-// function handleObserve(entries, observer) {
-
-//     entries.forEach(entry => {
-
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add("summary__box--highlight");
-//             entry.target.scrollIntoView(false);
-//         } 
-
-//         // else {
-//         //     entry.target.classList.remove("summary__box--highlight");
-//         // }
-        
-
-//     })
-
-// }
-
-// const observer = new IntersectionObserver(handleObserve, options);
-// observer.observe(summary);
-
-
-
-
 
 
 
