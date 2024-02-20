f72dc7ed4d81ce8f04645207a6464d7c13da3525
Leo-Code-CA <144139247+Leo-Code-CA@users.noreply.github.com>
2024-02-20 16:46:57 -0500

    design ongoing

---
+++ Js/treatments/category_page.js
@@ -0,0 +1,14 @@
+//////////////////////////// SERVICES PAGES ////////////////////////////
+
+// Imports
+
+// Media Queries
+
+// HTML Elements
+
+// Data
+
+// Variables
+
+
+
---
+++ Js/treatments/index.js
@@ -0,0 +1,5 @@
+// IMPORT ALL THE JS FILES NEEDED FOR THE SERVICES PAGES
+
+import '../footer.js';
+import '../header.js';
+import './category_page.js';
