cae5a1a6936e1f7ed1f213196f1dbac3b0843124
Leo-Code-CA <144139247+Leo-Code-CA@users.noreply.github.com>
2024-02-27 11:11:58 -0500

    found a version of the overview animation thats works, youhou

--- Js/treatments/massages.js
+++ Js/treatments/massages.js
@@ -10,127 +10,78 @@ const overview = document.querySelectorAll('.massages__overview');
 
 // Variables
 
-function handleOpenMassagesOverview(e) {
+function handleTargets(e) {
 
-    // Select the element that has been clicked AND the three others massage elements
-    const selectedMassage = e.currentTarget;
-    const activeInfo = selectedMassage.children[2];
+    const clickedTarget = e.target;
+    const clickHandler = e.currentTarget;
+    let currentlyOpen = false;
+    let selectedClose = false;
+    let selectedMassage;
+    let selectedMassageIndex;
     let notSelectedMassages;
-    overview.forEach((massage, key) => {
-        massage === selectedMassage ?
-        notSelectedMassages = document.querySelectorAll(`.massages__overview:not(:nth-child(${key + 1}))`)
-        : null;
-    });
 
-    const checkIfSelected = document.querySelector('.massages__overview--selected');
+    overview.forEach((massageElem, i) => {
 
-    if (!checkIfSelected) {
-
-        // Give the three elements not selected an opacity of 0
-        notSelectedMassages.forEach(massage => massage.style.opacity = 0);
-        // Add a class to the active massage element - it increases its size
-        selectedMassage.classList.add('massages__overview--selected');
-        // Show the info that were hidden
-        activeInfo.classList.remove("d-none");
-
-    } else {
-
-        // console.log(e.target, activeInfo.children[0].children[0])
-
-        if (e.target === activeInfo.children[0].children[0]) {
-            // Give the three elements that were hidden an opacity of 1
-            notSelectedMassages.forEach(massage => massage.style.opacity = 1);
-            // Remove the class of the massage element that was active - decreases its size
-            selectedMassage.classList.remove('massages__overview--selected');
-            // Hide the info that were showed when the massage element was selected
-            activeInfo.classList.add("d-none");
-        };
-
-        if (checkIfSelected && !checkIfSelected.contains(clickTarget)) {
-            console.log('outside')
+        if (massageElem.classList.contains('massages__overview--selected')) {
+            currentlyOpen = true;
         }
 
+        if (massageElem.contains(clickedTarget)) {
 
+            selectedMassage = massageElem;
+            selectedMassageIndex = i + 1;
+            notSelectedMassages = document.querySelectorAll(`.massages__overview:not(:nth-child(${i + 1}))`)
 
-    };
-    
-};
+            clickedTarget.classList.contains('massages__close') ? selectedClose = true : null;
 
-// Call the function to handle the overview on click
-// overview.forEach(massage => {
-//     massage.addEventListener('click', (e) => handleOpenMassagesOverview(e));
-// });
-
-
-
-window.addEventListener('click', (e) => {
-
-    handleMassageOverview(e);
-
-})
-
-function handleMassageOverview(e) {
-    // 3 cases: click on and open / click on cross and close / click outside and close if open
-    const clickedElem = e.target;
-    let massageOrInside = false;
-    let clickTarget;
-    const massageSelected = document.querySelector('.massages__overview--selected');
-    const closeELem = document.querySelector('.massages__close');
-    
-    overview.forEach(elem => {
-        if (!massageOrInside && elem === clickedElem) {
-            massageOrInside = true;
-            clickTarget = elem;
-            console.log("on massage")
         }
     });
 
-    if (!clickTarget && clickedElem.closest('.massages__overview')) {
-        console.log('inside it')
-        clickTarget = clickedElem;
-        massageOrInside = true;
+    if (!currentlyOpen && selectedMassage) {
+
+        const activeInfo = document.querySelector(`.massages__overview:nth-child(${selectedMassageIndex}) .massages__active`);
+
+        handleOpenOverview(selectedMassage, notSelectedMassages, activeInfo);
     }
 
-    if (massageOrInside) {
-        if (!massageSelected) {
-            console.log("open massage");
-        } else if (massageSelected && clickedElem === closeELem) {
-            console.log('close the popup from the cross')
-        }
-    } else {
-        if (massageSelected) {
-            console.log('close popup from outside')
-        }
+    if (currentlyOpen && selectedClose) {
+
+        const activeInfo = document.querySelector(`.massages__overview:nth-child(${selectedMassageIndex}) .massages__active`);
+
+        handleCloseOverview(selectedMassage, notSelectedMassages, activeInfo);
     }
-    
-    
 
-    // if (clickedElem.closest('.massages__overview')) return { target: clickedElem, click: 'inside' };
-
-    // return { target: clickedElem, click: 'outside' };
-
-
-
-
-
-    // const closeELem = document.querySelector('.massages__close');
-    // const selectedMassage = document.querySelector('.massages__overview--selected');
-    // const insideMassage = selectedMassage?.contains(e.target);
-
-    // if (insideMassage) {
-
-    //     if (selectedMassage && e.target === closeELem) {
-    //         console.log("close because cross clicked");
-    //     }
-
-    //     if (!selectedMassage) {
-    //         console.log('open the popup because clicked on it')
-    //     }
-
-    // } else if (!insideMassage && selectedMassage) {
-    //     console.log('close because clicked outisde')
-    // }
-
+    if (currentlyOpen && !selectedMassage) {
+        selectedMassage = document.querySelector('.massages__overview--selected');
+        notSelectedMassages = document.querySelectorAll(".massages__overview:not(.massages__overview--selected)");
+        const activeInfo = document.querySelector(".massages__overview--selected .massages__active");
+        // console.log(selectedMassage, notSelectedMassages, activeInfo)
+        handleCloseOverview(selectedMassage, notSelectedMassages, activeInfo);
+    }
 
 
 }
+
+function handleOpenOverview(selectedMassage, notSelectedMassages, activeInfo) {
+
+    // Give the three elements not selected an opacity of 0
+    notSelectedMassages.forEach(massage => massage.style.opacity = 0);
+    // Add a class to the active massage element - it increases its size
+    selectedMassage.classList.add('massages__overview--selected');
+    // Show the info that were hidden
+    activeInfo.classList.remove("d-none");
+
+}
+
+function handleCloseOverview(selectedMassage, notSelectedMassages, activeInfo) {
+
+    // Give the three elements that were hidden an opacity of 1
+    notSelectedMassages.forEach(massage => massage.style.opacity = 1);
+    // Remove the class of the massage element that was active - decreases its size
+    selectedMassage.classList.remove('massages__overview--selected');
+    // Hide the info that were showed when the massage element was selected
+    activeInfo.classList.add("d-none");
+
+}
+
+window.addEventListener('click', handleTargets);
