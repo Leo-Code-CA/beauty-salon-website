ac3ed33ea61d13825368b308fb11845031b4c07a
Leo-Code-CA <leonore.maciuga.ca@gmail.com>
2024-02-29 13:01:37 -0500

    massages page mobile version done - code cleanup in progress

--- Js/treatments/massages.js
+++ Js/treatments/massages.js
@@ -7,12 +7,10 @@ import slideInObserver from "./../utils/slideInObserver.js";
 
 // HTML Elements
 const root = document.querySelector(':root');
-const overview = document.querySelectorAll('.massages__overview');
-const overviewBtn = document.querySelectorAll('.massages__overview .massages__discoverBtn');
+const overview = document.querySelectorAll('.overview__card');
+const overviewBtn = document.querySelectorAll('.overview__card .overview__massageBtn');
 const doorIllustration = document.querySelector('.massages__door');
-const allMassagesDescription = document.querySelectorAll('.massages__service');
-// const doorImg = document.querySelector('.massages__door img');
-// const doorHiddenInfo = document.querySelector('.massages__hiddenInfo');
+const allMassagesDescription = document.querySelectorAll('.massages__service:not(:nth-child(1))');
 
 // Data
 
@@ -25,14 +23,14 @@ function handleOverviewTargets(e) {
 
     overview.forEach((massageElem, i) => {
 
-        const notSelectedMassages = document.querySelectorAll(`.massages__overview:not(:nth-child(${i + 1}))`);
-        const activeInfo = document.querySelector(`.massages__overview:nth-child(${i + 1}) .massages__active`);
+        const notSelectedMassages = document.querySelectorAll(`.overview__card:not(:nth-child(${i + 1}))`);
+        const activeInfo = document.querySelector(`.overview__card:nth-child(${i + 1}) .overview__active`);
 
         // handle case where one massage element is already open, so we eventually want to close it
-        if (massageElem.classList.contains('massages__overview--selected')) {
+        if (massageElem.classList.contains('overview__card--selected')) {
 
             // close that element if the user clicked outside of it or on the cross icon
-            if (!massageElem.contains(clickedTarget) || clickedTarget.classList.contains('massages__close')) {
+            if (!massageElem.contains(clickedTarget) || clickedTarget.classList.contains('overview__closeCard')) {
                 handleToggleOverview(massageElem, notSelectedMassages, activeInfo);
             }
 
@@ -49,11 +47,11 @@ function handleToggleOverview(selectedMassage, notSelectedMassages, activeInfo) 
     // Toggle not selected massages elements opacity between 0 and 1
     notSelectedMassages.forEach(massage => massage.classList.toggle('opacity-0'));
     // Toggle class of the selected element to increase or decrease its size
-    selectedMassage.classList.toggle('massages__overview--selected');
+    selectedMassage.classList.toggle('overview__card--selected');
     // Toggle class of one of the selected element child to show or hide its content
     activeInfo.classList.toggle("d-none");
     // Toggle the pulse animation on the first massage element
-    document.querySelector('.massages__overview:nth-child(1)').classList.toggle('massages__animatePulse');
+    document.querySelector('.overview__card:nth-child(1)').classList.toggle('massages__animatePulse');
 }
 
 window.addEventListener('click', handleOverviewTargets);
@@ -68,15 +66,17 @@ overviewBtn.forEach((btn, i) => {
 // HANDLE DOOR ILLUSTRATION REVEAL ANIMATION
 function handleDoorReveal() {
 
-    const hiddenInfoAnimName = window.getComputedStyle(root).getPropertyValue('--hiddenInfoAnimName');
+    const imgAnimName = window.getComputedStyle(root).getPropertyValue('--imgAnimName');
 
-    if (!hiddenInfoAnimName || hiddenInfoAnimName === 'hideContent') {
-        root.style.setProperty('--hiddenInfoAnimName', 'showContent');
+    if (imgAnimName === "moveTopLeft" || imgAnimName.includes('showContent')) {
+        root.style.setProperty('--hiddenInfoAnimName', 'moveBottomRight');
+        root.style.setProperty('--hiddenInfoAnimState', 'paused');
         root.style.setProperty('--imgAnimName', 'moveTopLeft, hideContent');
         root.style.setProperty('--imgAnimState', 'paused, running');
         root.style.setProperty('--doorShapeAnimState', 'paused');
-    } else if (hiddenInfoAnimName === 'showContent') {
-        root.style.setProperty('--hiddenInfoAnimName', 'hideContent');
+    } else if (!imgAnimName.includes('showContent')) {
+        root.style.setProperty('--hiddenInfoAnimName', 'moveBottomRight');
+        root.style.setProperty('--hiddenInfoAnimState', 'running');
         root.style.setProperty('--imgAnimName', 'moveTopLeft, showContent');
         root.style.setProperty('--imgAnimState', 'running');
         root.style.setProperty('--doorShapeAnimState', 'running');
@@ -87,5 +87,4 @@ function handleDoorReveal() {
 doorIllustration.addEventListener('click', handleDoorReveal);
 
 // HANDLE MASSAGES SLIDE IN ANIMATION
-// allMassagesDescription.forEach(massageElem => slideInObserver(massageElem, 'massages__slideInAnimation', { root: null, threshold: 0.3}));
-slideInObserver(document.querySelector('.massages__service'), 'massages__slideInAnimation', { root: null, threshold: [0, 1]});
+allMassagesDescription.forEach(massageElem => slideInObserver(massageElem, 'massages__slideInAnimation', { root: null, threshold: 0}));
--- Js/utils/slideInObserver.js
+++ Js/utils/slideInObserver.js
@@ -1,65 +1,25 @@
 import FnScrollDirection from './scrollDirection.js';
 
-// BELOW WORKS FINE WITH 0 THRESHOLD
-
-// export default function slideInObserver(elem, className, options = {root: null, threshold: 0}) {
-
-//     const observer = new IntersectionObserver(handleIntersect, options);
-    
-//     function handleIntersect(entries) {
-//         entries.forEach(entry => {
-//             if (entry.isIntersecting) {
-//                 const scrollDirection = FnScrollDirection(window.scrollY);
-//                 scrollDirection === "down" ? entry.target.classList.add(className) : entry.target.classList.remove(className);
-//             };
-//         });
-//     };
-
-//     observer.observe(elem);
-
-// };
-
-// TEST WITH GOAL TO IMPROVE THE VISIBILITY OF THE ELEMENTS BEFORE AND AFTER THE FIRST FULL PAGE SCROLL
-
 export default function slideInObserver(elem, className, options = {root: null, threshold: 0}) {
 
     const observer = new IntersectionObserver(handleIntersect, options);
     
     function handleIntersect(entries) {
-
         entries.forEach(entry => {
-
-            const heightFromTopDoc = entry.boundingClientRect.top + window.scrollY;
-            const currentScrollPosition = window.scrollY;
-
-            console.log(currentScrollPosition <= heightFromTopDoc)
-            console.log(entry)
-
-
-            if (entry.isIntersecting) {
-                const scrollDirection = FnScrollDirection(window.scrollY);
-                scrollDirection === "down" ? entry.target.classList.add(className) : entry.target.classList.remove(className);
+            const scrollDirection = FnScrollDirection(window.scrollY);
+            if (entry.isIntersecting && scrollDirection === "down") {
+                entry.target.classList.add(className);
+            } else if (entry.boundingClientRect.top > 0 && scrollDirection === "up") {
+               entry.target.classList.remove(className);   
             };
-
-
         });
-
-
     };
-
     observer.observe(elem);
-
 };
-
-
-
 
 // INTERESTING CODE FROM MDN:
 // function intersectionCallback(entries) {
 //     entries.forEach((entry) => {
 //       entry.target.style.opacity = entry.intersectionRatio;
 //     });
-//   }
-
-// https://stackoverflow.com/questions/58980851/detect-whether-element-is-above-or-below-the-viewport-on-intersect-leave-with-in
-// check that out to see if the elem is above  or under! Good luck :D
+// }
