8fb908495160c314e8fbd8d3948d9f7d2b032c15
Leo-Code-CA <leonore.maciuga.ca@gmail.com>
2024-03-02 21:07:26 -0500

    image gallery functionnalities ok

--- Js/about/data.js
+++ Js/about/data.js
@@ -2,61 +2,76 @@ export const images = [
     {
         src: 'images/about/lobby_salon_decor_image.jpeg',
         alt: 'salon decor image',
-    },
-    {
-        src: 'images/about/lobby_salon_door_image.jpeg',
-        alt: 'salon entrance door image',
-    },
-    {
-        src: 'images/about/lobby_salon_entrance_image.jpeg',
-        alt: 'salon lobby entrance image',
-    },
-    {
-        src: 'images/about/lobby_salon_waitroom_cosy_image.jpeg',
-        alt: 'salon wait room image',
-    },
-    {
-        src: 'images/about/lobby_salon_waitroom_image.jpeg',
-        alt: 'salon wait room image',
-    },
-    {
-        src: 'images/about/lobby_salon_waitroom_window_image.jpeg',
-        alt: 'salon wait room window image',
-    },
-    {
-        src: 'images/about/massage_room1_details_image.jpeg',
-        alt: 'salon first massage room image',
-    },
-    {
-        src: 'images/about/massage_room1_overview_image.jpeg',
-        alt: 'salon first massage room image',
+        type: 'lobby'
     },
     {
         src: 'images/about/massage_room1_pink_image.jpeg',
         alt: 'salon first massage room image',
+        type: 'massage'
     },
     {
-        src: 'images/about/massage_room2_image.jpeg',
-        alt: 'salon second massage room image',
+        src: 'images/about/lobby_salon_waitroom_cosy_image.jpeg',
+        alt: 'salon wait room image',
+        type: 'lobby'
     },
     {
-        src: 'images/about/massage_table_image.jpeg',
-        alt: 'salon massage table image',
-    },
-    {
-        src: 'images/about/products_display_image.jpeg',
-        alt: 'salon products display image',
-    },
-    {
-        src: 'images/about/products_heliabrine_image.jpeg',
-        alt: 'salon products display image',
-    },
-    {
-        src: 'images/about/products_novexpert_image.jpeg',
-        alt: 'salon products display image',
+        src: 'images/about/lobby_salon_door_image.jpeg',
+        alt: 'salon entrance door image',
+        type: 'lobby'
     },
     {
         src: 'images/about/products_perronrigot_image.jpeg',
         alt: 'salon products display image',
+        type: 'products'
+    },
+    {
+        src: 'images/about/massage_room2_image.jpeg',
+        alt: 'salon second massage room image',
+        type: 'massage'
+    },
+    {
+        src: 'images/about/lobby_salon_entrance_image.jpeg',
+        alt: 'salon lobby entrance image',
+        type: 'lobby'
+    },
+    {
+        src: 'images/about/lobby_salon_waitroom_window_image.jpeg',
+        alt: 'salon wait room window image',
+        type: 'lobby'
+    },
+    {
+        src: 'images/about/massage_room1_details_image.jpeg',
+        alt: 'salon first massage room image',
+        type: 'massage'
+    },
+    {
+        src: 'images/about/products_display_image.jpeg',
+        alt: 'salon products display image',
+        type: 'products'
+    },
+    {
+        src: 'images/about/massage_room1_overview_image.jpeg',
+        alt: 'salon first massage room image',
+        type: 'massage'
+    },
+    {
+        src: 'images/about/lobby_salon_waitroom_image.jpeg',
+        alt: 'salon wait room image',
+        type: 'lobby'
+    },
+    {
+        src: 'images/about/products_heliabrine_image.jpeg',
+        alt: 'salon products display image',
+        type: 'products'
+    },
+    {
+        src: 'images/about/massage_table_image.jpeg',
+        alt: 'salon massage table image',
+        type: 'massage'
+    },
+    {
+        src: 'images/about/products_novexpert_image.jpeg',
+        alt: 'salon products display image',
+        type: 'products'
     }
 ];
--- Js/about/gallery.js
+++ Js/about/gallery.js
@@ -5,40 +5,120 @@ import { images } from './data.js';
 // Media Queries
 
 // HTML Elements
-
+const imgContainer = document.querySelector('.gallery__imgContainer');
+const filterBtn = document.querySelectorAll('.gallery__filter');
 // Data
 
 // Variables
 
+// create the img elements
 function createImgElem(column, src, alt) {
 
+    const imgWrapper = document.createElement('span');
     const img = document.createElement('img');
     img.setAttribute('src', src);
     img.setAttribute('alt', alt);
-    img.classList.add('gallery__img');
-    column.appendChild(img);
+    imgWrapper.appendChild(img);
+    column.appendChild(imgWrapper);
+    img.addEventListener('click', (e) => handleImgZoom(e));
 
 }
 
-function createImgLayout() {
+// Create the actual gallery layout columns
+function createImgLayout(col, imgCollection) {
 
-    const totalImg = images.length; // 15
-    const imgPerColumn = Math.round(totalImg / 4); // 4
+    // cleanup by removing the current html if any
+    if (imgContainer.innerHTML) {
+        while (imgContainer.firstChild) {
+            imgContainer.removeChild(imgContainer.firstChild);
+        }
+    }
+
+    // create the number of columns needed
+    let i = 0;
+    let totalColumns = [];
+
+    while (i < col) {
+        const newColumn = document.createElement('div');
+        newColumn.classList.add('gallery__column');
+        imgContainer.appendChild(newColumn);
+        totalColumns.push(newColumn);
+        i++;
+    }
+
+    // depending on the total number of images, determine how many image will be in each column and create them
+    const totalImg = imgCollection.length; 
+    const imgPerColumn = Math.round(totalImg / col); 
     let currentLimit = imgPerColumn; 
     let currentColumn = 1;
 
     for (let i = 0; i < totalImg; i++) {
     
-        // console.log(i, currentLimit, currentColumn)
         if (i === currentLimit) {
-            console.log(i, currentLimit, currentColumn)
             currentLimit += imgPerColumn;
             currentColumn++; 
         }
 
-        const currentlyFilling = document.querySelector(`.gallery__column:nth-child(${currentColumn})`);
-        createImgElem(currentlyFilling, images[i].src, images[i].alt)
+        createImgElem(totalColumns[currentColumn - 1], imgCollection[i].src, imgCollection[i].alt)
     }
 }
 
-createImgLayout();
+// Choose the numbers of columns for the gallery layout depending on the viewport width
+function handleWindowSize(imgCollection) {
+
+    const vw = window.innerWidth;
+
+    return vw <= 768 ? createImgLayout(2, imgCollection)
+    : vw <= 1200 ? createImgLayout(3, imgCollection)
+    : vw > 1200 ? createImgLayout(4, imgCollection)
+    : new Error("vw mush be a number and must be defined");
+
+}
+
+window.addEventListener('load', () => handleWindowSize(images));
+window.addEventListener('resize', () => handleWindowSize(images));
+
+// Handle gallery filtering 
+function handleGalleryFilter(filter) {
+
+    switch (filter) {
+        case "all":
+            handleWindowSize(images);
+            break;
+        case "lobby":
+            handleWindowSize(images.filter(img => img.type === "lobby"));
+            break;
+        case "massage":
+            handleWindowSize(images.filter(img => img.type === "massage"));
+            break;
+        case "products":
+            handleWindowSize(images.filter(img => img.type === "products"));
+            break;
+        default:
+            throw new Error('filter not reconized!')
+    }
+
+
+}
+
+filterBtn.forEach(btn => btn.addEventListener('click', (e) => handleGalleryFilter(e.target.id)));
+
+// Handle img zoom and background change on click
+function handleImgZoom(e) {
+
+    const selectedImg = e.target;
+    const selectedImgBox = selectedImg.parentElement;
+
+    selectedImg.classList.add('gallery--imgPopup');
+    selectedImgBox.classList.add('gallery--bgPopup');
+    selectedImgBox.addEventListener('click', (e) => {
+
+        if (e.target === selectedImgBox) {
+            selectedImg.classList.remove('gallery--imgPopup');
+            selectedImgBox.classList.remove('gallery--bgPopup');
+        }
+    })
+
+};
+
+
